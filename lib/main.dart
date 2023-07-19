eb6e44f5038558628ef88ae814dce8997d0d82a7
FNicoll <nicolle.castellano@unah.hn>
2023-07-18 23:54:00 -0600

    Listo

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Home(),
+      home: const Home(),
     );
   }
 }
