e8970f844ebde6ef1030cfaa2fd59f526b40283e
FNicoll <nicolle.castellano@unah.hn>
2023-07-18 23:53:09 -0600

    Correccion de errores

--- lib/cuadrito.dart
+++ lib/cuadrito.dart
@@ -15,7 +15,13 @@ class Cuadrito extends StatelessWidget {
               padding: EdgeInsets.only(bottom: 9, left: 11),
               child: Row(
                 children: [
-                  Text('Informacion de contacto'),
+                  Text(
+                    'Informacion de contactos',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -24,7 +30,7 @@ class Cuadrito extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               children: [
                 Padding(
-                  padding: EdgeInsets.only(left: 9, right: 20),
+                  padding: EdgeInsets.only(left: 13, right: 25),
                   child: Icon(Icons.phone_outlined, size: 30),
                 ),
                 Column(
@@ -35,65 +41,47 @@ class Cuadrito extends StatelessWidget {
                     Text(
                       "9902-9311",
                       style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "Celular",
+                      style: TextStyle(
                         fontSize: 14,
                         color: Color.fromARGB(255, 118, 118, 118),
                         fontWeight: FontWeight.w400,
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(left: 9, right: 20),
-                          child: Icon(Icons.phone_outlined, size: 30),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Text(
-                              "9902-9311",
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Color.fromARGB(255, 118, 118, 118),
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Spacer(),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Icon(
-                                Icons.video_call_outlined,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Icon(
-                                Icons.message_outlined,
-                                size: 30,
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
                   ],
                 ),
+                Spacer(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(
+                        Icons.video_call_outlined,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(
+                        Icons.message_outlined,
+                        size: 30,
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-            Filitas()
+            SizedBox(
+              height: 8,
+            ),
+            Filitas(),
           ],
         ),
       ),
--- lib/home.dart
+++ lib/home.dart
@@ -2,49 +2,51 @@ import "package:contacto/cuadrito.dart";
 import "package:contacto/widgets/iconos.dart";
 import "package:flutter/material.dart";
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Align(
-      alignment: Alignment.center,
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 55.0),
-            child: CircleAvatar(
-              radius: 45,
-              backgroundColor: Color.fromARGB(255, 244, 3, 99),
-              child: Text(
-                'P',
-                style: TextStyle(fontSize: 55, color: Colors.white),
-              ),
-            ),
-          ),
-          Text(
-            'Pascualillo',
-            style: TextStyle(
-              fontSize: 27,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 13),
-          Divider(),
-          Iconos(),
-          Divider(),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Cuadrito(),
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tarea"),
       ),
-    ));
+      body: const SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 55.0),
+                child: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 244, 3, 99),
+                  child: Text(
+                    'P',
+                    style: TextStyle(fontSize: 55, color: Colors.white),
+                  ),
+                ),
+              ),
+              Text(
+                'Pascualillo',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 13),
+              Divider(),
+              Iconos(),
+              Divider(),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Cuadrito(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
